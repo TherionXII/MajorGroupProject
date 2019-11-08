e9c395980fd6f2d8da542e4f57f67931f18c2912
TherionXI <n.savkovs@gmail.com>
2019-11-08 11:14:06 +0000

    Created back-end project structure

---
+++ Web Collaboration Tool/Back-end/src/main/java/project/webcollaborationtool/WebCollaborationToolApplication.java
@@ -0,0 +1,13 @@
+package project.webcollaborationtool;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebCollaborationToolApplication
+{
+    public static void main(String[] args)
+    {
+        SpringApplication.run(WebCollaborationToolApplication.class, args);
+    }
+}
