41371f57b858119069080066089ac6d65203ed4a
Colm Doggett <colmdoggett78@gmail.com>
2019-11-30 12:40:24 +0000

    Updated to reflect new UserInformation

--- Web Collaboration Tool/Back-end/src/main/java/project/webcollaborationtool/Controllers/UserController.java
+++ Web Collaboration Tool/Back-end/src/main/java/project/webcollaborationtool/Controllers/UserController.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import project.webcollaborationtool.Entities.User;
+import project.webcollaborationtool.Entities.UserInformation;
 import project.webcollaborationtool.Services.UserService;
 
 @RestController
@@ -17,6 +18,28 @@ public class UserController
     public ResponseEntity<String> createUser(@RequestBody User user)
     {
         this.userService.createUser(user);
+        return ResponseEntity.ok().build();
+    }
+
+
+    public ResponseEntity<String> createUserInformation(@RequestBody UserInformation userInformation)
+    {
+        this.userService.createUserInformation(userInformation);
+        return ResponseEntity.ok().build();
+    }
+
+    @PostMapping(path = "/{username}/changePassword")
+    public ResponseEntity<String> changePassword(@PathVariable String username, @RequestBody String password)
+    {
+        this.userService.changePassword(username ,password);
+        return ResponseEntity.ok().build();
+    }
+
+    @PostMapping(path = "/{username}/changeUserInformation")
+    public ResponseEntity<String> changeUserInformation(@PathVariable User username,
+                                                        @RequestBody UserInformation userInformation)
+    {
+        this.userService.changeUserInformation(username, userInformation);
         return ResponseEntity.ok().build();
     }
 }
