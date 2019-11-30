13dfa96121c39de2b20ff9624762374caae127c0
Colm Doggett <colmdoggett78@gmail.com>
2019-11-30 12:43:49 +0000

    Added new methods createUserInformation, changePassword and changeUserInformation

--- Web Collaboration Tool/Back-end/src/main/java/project/webcollaborationtool/Services/UserService.java
+++ Web Collaboration Tool/Back-end/src/main/java/project/webcollaborationtool/Services/UserService.java
@@ -3,6 +3,8 @@ package project.webcollaborationtool.Services;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import project.webcollaborationtool.Entities.User;
+import project.webcollaborationtool.Entities.UserInformation;
+import project.webcollaborationtool.Repositories.UserInformationRepository;
 import project.webcollaborationtool.Repositories.UserRepository;
 
 import javax.validation.constraints.NotNull;
@@ -13,8 +15,31 @@ public class UserService
     @Autowired
     private UserRepository userRepository;
 
+    @Autowired
+    private UserInformationRepository userInformationRepository;
+
     public void createUser(@NotNull User user)
     {
         this.userRepository.save(user);
     }
+
+    public void createUserInformation(@NotNull UserInformation userInformation)
+    {
+        this.userInformationRepository.save(userInformation);
+    }
+
+    public void changePassword(@NotNull String username, @NotNull String password)
+    {
+        var user = this.userRepository.findByUsername(username);
+        user.setPassword(password);
+        this.userRepository.save(user);
+    }
+
+    public void changeUserInformation(@NotNull User username, @NotNull UserInformation userInformation)
+    {
+        var updatedUserInformation = userInformationRepository.findByUsername(username);
+        updatedUserInformation.setName(userInformation.getName());
+        this.userInformationRepository.save(userInformation);
+    }
+
 }
