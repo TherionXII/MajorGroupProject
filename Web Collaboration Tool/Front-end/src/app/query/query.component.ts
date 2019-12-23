b2a76fdb9501d266edd2b09c4c37c25168d47ca3
Nick <n.savkovs@gmail.com>
2019-12-23 19:55:57 +0000

    Updated query page

--- Web Collaboration Tool/Front-end/src/app/query/query.component.ts
+++ Web Collaboration Tool/Front-end/src/app/query/query.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import {QueryService} from '../services/query.service';
 import {ActivatedRoute} from '@angular/router';
 import {IQuery} from '../Interfaces/IQuery';
+import {FormGroup} from '@angular/forms';
 
 @Component({
   selector: 'app-query',
@@ -10,13 +11,26 @@ import {IQuery} from '../Interfaces/IQuery';
 })
 export class QueryComponent implements OnInit {
   public query: IQuery;
+  public username: string;
+
+  public isReplyVisible = false;
+
+  public queryResponse: FormGroup;
 
   constructor(private queryService: QueryService,
               private activatedRoute: ActivatedRoute) { }
 
   ngOnInit() {
+    this.username = localStorage.getItem('username');
     this.queryService.getQueryById(this.activatedRoute.snapshot.paramMap.get('id') as unknown as number)
       .subscribe(result => this.query = result, error => console.log(error));
+
+    // this.queryResponse = new FormGroup({
+    //   response: Form
+    // })
   }
 
+  public onRespond(): void {
+    this.isReplyVisible = !this.isReplyVisible;
+  }
 }
