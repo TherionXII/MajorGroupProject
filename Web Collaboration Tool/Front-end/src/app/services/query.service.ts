62989d2041cea7401d18923e40759e2f743e1ad7
Nick <n.savkovs@gmail.com>
2020-01-03 19:34:49 +0000

    Updated user page to include most recent queries and responses

--- Web Collaboration Tool/Front-end/src/app/services/query.service.ts
+++ Web Collaboration Tool/Front-end/src/app/services/query.service.ts
@@ -12,26 +12,34 @@ export class QueryService {
   constructor(private httpClient: HttpClient) { }
 
   public getRecentQueries(): Observable<IQuery[]> {
-    return this.httpClient.get<IQuery[]>('http://localhost:8080/getRecentQueries');
+    return this.httpClient.get<IQuery[]>(`http://localhost:8080/getRecentQueries`);
+  }
+
+  public getRecentQueriesForUser(username: string): Observable<IQuery[]> {
+    return this.httpClient.get<IQuery[]>(`http://localhost:8080/${username}/getRecentQueries`);
+  }
+
+  public getRecentResponsesForUser(username: string): Observable<number[]> {
+    return this.httpClient.get<number[]>(`http://localhost:8080/${username}/getRecentResponses`);
   }
 
   public createParentQuery(queryData: IQueryData, username: string): Observable<number> {
-    return this.httpClient.post<number>('http://localhost:8080/' + username + '/createParentQuery', queryData);
+    return this.httpClient.post<number>(`http://localhost:8080/${username}/createParentQuery`, queryData);
   }
 
   public createParentQueryData(parentQueryData: IParentQueryData, id: number): Observable<string> {
-    return this.httpClient.post<string>('http://localhost:8080/' + id + '/createParentQueryData', parentQueryData);
+    return this.httpClient.post<string>(`http://localhost:8080/${id}/createParentQueryData`, parentQueryData);
   }
 
   public getQueryById(id: number): Observable<IQuery> {
-    return this.httpClient.get<IQuery>('http://localhost:8080/getQuery/' + id);
+    return this.httpClient.get<IQuery>(`http://localhost:8080/getQuery/${id}`);
   }
 
   public submitResponse(response: string, username: string, id: number): Observable<string> {
-    return this.httpClient.post<string>('http://localhost:8080/' + username + '/' + id + '/submitResponse', response);
+    return this.httpClient.post<string>(`http://localhost:8080/${username}/${id}/submitResponse`, response);
   }
 
   public submitVote(vote: boolean, username: string, id: number): Observable<IQuery> {
-    return this.httpClient.post<IQuery>('http://localhost:8080/' + username + '/' + id + '/vote', vote);
+    return this.httpClient.post<IQuery>(`http://localhost:8080/${username}/${id}/vote`, vote);
   }
 }
--- Web Collaboration Tool/Front-end/src/app/user-page/user-page.component.ts
+++ Web Collaboration Tool/Front-end/src/app/user-page/user-page.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import {UserService} from '../services/user.service';
 import {IUserProfile} from '../Interfaces/IUserProfile';
 import {ActivatedRoute} from '@angular/router';
+import {QueryService} from '../services/query.service';
+import {IQuery} from '../Interfaces/IQuery';
 
 @Component({
   selector: 'app-user-page',
@@ -10,19 +12,35 @@ import {ActivatedRoute} from '@angular/router';
 })
 export class UserPageComponent implements OnInit {
   public username: string;
-  public userInformation: IUserProfile = {
-    name: '',
-    surname: '',
-    gender: '',
-    institution: ''
-  };
+  public userInformation: IUserProfile;
+
+  public userQueries: Array<IQuery>;
+  public userResponses: Array<IQuery>;
 
   constructor(private userService: UserService,
+              private queryService: QueryService,
               private route: ActivatedRoute) { }
 
   ngOnInit() {
     this.userService.getUserProfile(this.route.snapshot.paramMap.get('username'))
       .subscribe(response => this.userInformation = response, error => console.log(error));
     this.username = localStorage.getItem('username');
+
+    this.queryService.getRecentQueriesForUser(this.username)
+      .subscribe(queries => this.userQueries = queries, error => console.log(error));
+
+    this.userResponses = new Array<IQuery>();
+    this.queryService.getRecentResponsesForUser(this.username)
+      .subscribe(responses => responses.forEach(id => this.queryService.getQueryById(id)
+        .subscribe(response => this.userResponses.push(response), error => console.log(error))),
+          error => console.log(error));
+  }
+
+  public getParent(response: IQuery): IQuery {
+    while (response.parent != null) {
+      response = response.parent;
+    }
+
+    return response;
   }
 }
