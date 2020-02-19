2453c2318db11bd2ad5f36ef797a936dadca746d
Nick <n.savkovs@gmail.com>
2020-02-19 12:37:39 +0000

    Updated tests for User-related classes

--- Web Collaboration Tool/Back-end/src/test/java/project/webcollaborationtool/User/Services/ProfileServiceTests.java
+++ Web Collaboration Tool/Back-end/src/test/java/project/webcollaborationtool/User/Services/ProfileServiceTests.java
@@ -31,7 +31,7 @@ public class ProfileServiceTests
     public void testUpdateProfileWithValidData()
     {
         var profile = new Profile();
-        var user = new User("username", "password", "email", null);
+        var user = new User("username", "password", "email", null, null);
 
         when(this.userRepository.existsById("username")).thenReturn(true);
         when(this.profileRepository.existsById("username")).thenReturn(true);
@@ -61,7 +61,7 @@ public class ProfileServiceTests
     @Test
     public void testGetProfileWithValidData()
     {
-        var user = new User("username", "password", "email", new Profile());
+        var user = new User("username", "password", "email", new Profile(), null);
 
         when(this.userRepository.existsById("username")).thenReturn(true);
         when(this.profileRepository.existsById("username")).thenReturn(true);
