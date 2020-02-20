5c1c69aa3418b2128af7f95500a4637b305fe1ed
Nick <n.savkovs@gmail.com>
2020-02-20 15:40:42 +0000

    Updated UserPageComponent tests to reflect changes in the UserPageComponent; implemnted tests for the resolvers

--- Web Collaboration Tool/Front-end/src/app/user-feature/user-page/user-page.component.spec.ts
+++ Web Collaboration Tool/Front-end/src/app/user-feature/user-page/user-page.component.spec.ts
@@ -1,24 +1,36 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import {async, ComponentFixture, fakeAsync, TestBed, tick} from '@angular/core/testing';
 
 import { UserPageComponent } from './user-page.component';
 import {UserFeatureModule} from '../user-feature.module';
 import {UserService} from '../Services/user.service';
 import {QueryService} from '../../query-feature/services/query.service';
 import {RouterTestingModule} from '@angular/router/testing';
-import {ActivatedRoute} from '@angular/router';
+import {ActivatedRoute, ActivatedRouteSnapshot, Data, Router} from '@angular/router';
 import {Type} from '@angular/core';
-import {of, throwError} from 'rxjs';
+import {of, pipe, throwError} from 'rxjs';
 import {IUserProfile} from '../Interfaces/IUserProfile';
 import {IQuery} from '../../query-feature/Interfaces/IQuery';
+import {mergeMap, take} from 'rxjs/operators';
+import {ProfileResolverService} from '../../Utility/Resolvers/profile-resolver.service';
 
 describe('UserPageComponent', () => {
   let component: UserPageComponent;
   let fixture: ComponentFixture<UserPageComponent>;
 
+  let routeStub;
+
   const userService = jasmine.createSpyObj('UserService', [ 'getUserProfile' ]);
   const queryService = jasmine.createSpyObj('QueryService', [ 'getRecentQueriesForUser', 'getRecentResponsesForUser' ]);
 
-  beforeEach(async(() => {
+  beforeEach(() => {
+    routeStub = {
+      data: of({
+        userProfile: { name: 'name', surname: 'surname' } as IUserProfile,
+        queries: [ { title: 'title' } as IQuery, {} as IQuery ],
+        responses: [ { parent: { user: { username: 'username' } } } as IQuery ]
+      })
+    };
+
     TestBed.configureTestingModule({
       imports: [
         UserFeatureModule,
@@ -26,154 +38,39 @@ describe('UserPageComponent', () => {
       ],
       providers: [
         { provide: UserService, useValue: userService },
-        { provide: QueryService, useValue: queryService }
+        { provide: QueryService, useValue: queryService },
+        { provide: ActivatedRoute, useValue: routeStub }
       ]
     })
     .compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(UserPageComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', async(() => {
+    expect(component).toBeTruthy();
   }));
 
-  it('should create', () => {
-    spyOn(TestBed.inject(ActivatedRoute as Type<ActivatedRoute>).snapshot.paramMap, 'get').and.returnValue('username');
-
-    userService.getUserProfile.and.returnValue(of({} as IUserProfile));
-    queryService.getRecentQueriesForUser.and.returnValue(of([ { user: { username: 'username' } } as IQuery ]));
-    queryService.getRecentResponsesForUser.and.returnValue(of([ { parent: { user: { username: 'username' } } } as IQuery ]));
-
-    fixture = TestBed.createComponent(UserPageComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-
-    expect(component).toBeTruthy();
-  });
-
-  it('should have username set to the parameter from the URL', () => {
-    const getSpy = spyOn(TestBed.inject(ActivatedRoute as Type<ActivatedRoute>).snapshot.paramMap, 'get').and.returnValue('username');
-
-    userService.getUserProfile.and.returnValue(of({} as IUserProfile));
-    queryService.getRecentQueriesForUser.and.returnValue(of([ { user: { username: 'username' } } as IQuery ]));
-    queryService.getRecentResponsesForUser.and.returnValue(of([ { parent: { user: { username: 'username' } } } as IQuery ]));
-
-    fixture = TestBed.createComponent(UserPageComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-
-    expect(component.username).toEqual('username');
-    expect(getSpy).toHaveBeenCalledWith('username');
-  });
-
-  it('should retrieve the profile of the user with the username as in URL', () => {
-    const getSpy = spyOn(TestBed.inject(ActivatedRoute as Type<ActivatedRoute>).snapshot.paramMap, 'get').and.returnValue('username');
-
-    const getUserProfileSpy = userService.getUserProfile.and.returnValue(of({ name: 'name', surname: 'surname' } as IUserProfile));
-    queryService.getRecentQueriesForUser.and.returnValue(of([ { user: { username: 'username' } } as IQuery ]));
-    queryService.getRecentResponsesForUser.and.returnValue(of([ { parent: { user: { username: 'username' } } } as IQuery ]));
-
-    fixture = TestBed.createComponent(UserPageComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-
-    expect(getSpy).toHaveBeenCalledWith('username');
-    expect(getUserProfileSpy).toHaveBeenCalledWith('username');
+  it('should have resolved the profile', () => {
     expect(component.userProfile.name).toEqual('name');
     expect(component.userProfile.surname).toEqual('surname');
   });
 
-  it('should set an error when failed to retrieve user profile', () => {
-    const getSpy = spyOn(TestBed.inject(ActivatedRoute as Type<ActivatedRoute>).snapshot.paramMap, 'get').and.returnValue('username');
-
-    const getUserProfileSpy = userService.getUserProfile.and.returnValue(throwError({ message: 'error' }));
-    queryService.getRecentQueriesForUser.and.returnValue(of([ { user: { username: 'username' } } as IQuery ]));
-    queryService.getRecentResponsesForUser.and.returnValue(of([ { parent: { user: { username: 'username' } } } as IQuery ]));
-
-    fixture = TestBed.createComponent(UserPageComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-
-    expect(getSpy).toHaveBeenCalledWith('username');
-    expect(getUserProfileSpy).toHaveBeenCalledWith('username');
-    expect(component.userProfile).toEqual(undefined);
-    expect(component.initError).toEqual('error');
+  it('should have resolved the queries', () => {
+    expect(component.userQueries.length).toEqual(2);
+    expect(component.userQueries[0].title).toEqual('title');
   });
 
-  it('should retrieve recent queries of the user with the username as in URL', () => {
-    const getSpy = spyOn(TestBed.inject(ActivatedRoute as Type<ActivatedRoute>).snapshot.paramMap, 'get').and.returnValue('username');
-
-    userService.getUserProfile.and.returnValue(of({ name: 'name', surname: 'surname' } as IUserProfile));
-    const getRecentQueriesSpy = queryService.getRecentQueriesForUser.and.returnValue(of([ { user: { username: 'username' } } as IQuery ]));
-    queryService.getRecentResponsesForUser.and.returnValue(of([ { parent: { user: { username: 'username' } } } as IQuery ]));
-
-    fixture = TestBed.createComponent(UserPageComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-
-    expect(getSpy).toHaveBeenCalledWith('username');
-    expect(getRecentQueriesSpy).toHaveBeenCalledWith('username');
-    expect(component.userQueries.length).toEqual(1);
-    expect(component.userQueries[0].user.username).toEqual('username');
-  });
-
-  it('should set an error message when failed to retrieve queries of the user', () => {
-    const getSpy = spyOn(TestBed.inject(ActivatedRoute as Type<ActivatedRoute>).snapshot.paramMap, 'get').and.returnValue('username');
-
-    userService.getUserProfile.and.returnValue(of({ name: 'name', surname: 'surname' } as IUserProfile));
-    const getRecentQueriesSpy = queryService.getRecentQueriesForUser.and.returnValue(throwError({ message: 'error' }));
-    queryService.getRecentResponsesForUser.and.returnValue(of([ { parent: { user: { username: 'username' } } } as IQuery ]));
-
-    fixture = TestBed.createComponent(UserPageComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-
-    expect(getSpy).toHaveBeenCalledWith('username');
-    expect(getRecentQueriesSpy).toHaveBeenCalledWith('username');
-    expect(component.userQueries).toEqual(undefined);
-    expect(component.initError).toEqual('error');
-  });
-
-  it('should retrieve recent responses of the user with the username as in URL', () => {
-    const getSpy = spyOn(TestBed.inject(ActivatedRoute as Type<ActivatedRoute>).snapshot.paramMap, 'get').and.returnValue('username');
-
-    userService.getUserProfile.and.returnValue(of({ name: 'name', surname: 'surname' } as IUserProfile));
-    queryService.getRecentQueriesForUser.and.returnValue(of([ { user: { username: 'username' } } as IQuery ]));
-    const getRecentResponsesSpy = queryService.getRecentResponsesForUser.and.returnValue(of([ { parent: { user: { username: 'username' } } } as IQuery ]));
-
-    fixture = TestBed.createComponent(UserPageComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-
-    expect(getSpy).toHaveBeenCalledWith('username');
-    expect(getRecentResponsesSpy).toHaveBeenCalledWith('username');
+  it('should have resolved the responses', () => {
     expect(component.userResponses.length).toEqual(1);
     expect(component.userResponses[0].parent.user.username).toEqual('username');
   });
 
-  it('should set an error message when failed to retrieve responses of the user', () => {
-    const getSpy = spyOn(TestBed.inject(ActivatedRoute as Type<ActivatedRoute>).snapshot.paramMap, 'get').and.returnValue('username');
-
-    userService.getUserProfile.and.returnValue(of({ name: 'name', surname: 'surname' } as IUserProfile));
-    queryService.getRecentQueriesForUser.and.returnValue(of([ { user: { username: 'username' } } as IQuery ]));
-    const getRecentResponsesSpy = queryService.getRecentResponsesForUser.and.returnValue(throwError({ message: 'error' }));
-
-    fixture = TestBed.createComponent(UserPageComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-
-    expect(getSpy).toHaveBeenCalledWith('username');
-    expect(getRecentResponsesSpy).toHaveBeenCalledWith('username');
-    expect(component.userResponses).toEqual(undefined);
-    expect(component.initError).toEqual('error');
-  });
-
   it('should return the username of the owner of the parent query', () => {
-    spyOn(TestBed.inject(ActivatedRoute as Type<ActivatedRoute>).snapshot.paramMap, 'get').and.returnValue('username');
-
-    userService.getUserProfile.and.returnValue(of({} as IUserProfile));
-    queryService.getRecentQueriesForUser.and.returnValue(of([ { user: { username: 'username' } } as IQuery ]));
-    queryService.getRecentResponsesForUser.and.returnValue(of([ { parent: { user: { username: 'username' } } } as IQuery ]));
-
-    fixture = TestBed.createComponent(UserPageComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-
     const query = { parent: { parent: { user: { username: 'username' } } } };
 
     expect(component.getOwnerUsername(query as IQuery)).toEqual('username');
