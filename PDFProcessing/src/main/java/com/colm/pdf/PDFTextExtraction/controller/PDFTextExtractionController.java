4d5434ebeb8d1c5b49ebc01dca6a0e688e94c7d7
Colm Doggett <colmdoggett78@gmail.com>
2020-02-26 23:30:14 +0000

    Added selection of individual page. Only text from the page selected and inside the crop area is extracted.

--- PDFProcessing/src/main/java/com/colm/pdf/PDFTextExtraction/controller/PDFTextExtractionController.java
+++ PDFProcessing/src/main/java/com/colm/pdf/PDFTextExtraction/controller/PDFTextExtractionController.java
@@ -7,10 +7,8 @@ import org.apache.pdfbox.pdmodel.PDDocument;
 import org.apache.pdfbox.pdmodel.PDPage;
 import org.apache.pdfbox.rendering.ImageType;
 import org.apache.pdfbox.rendering.PDFRenderer;
-import org.apache.pdfbox.text.PDFTextStripper;
 import org.apache.pdfbox.text.PDFTextStripperByArea;
 import org.json.JSONObject;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Component;
@@ -31,26 +29,20 @@ public class PDFTextExtractionController
     @PostMapping("/pdf/textExtraction")
     @CrossOrigin
     public ResponseEntity<String>extractTextFromPDF(@RequestParam("file") MultipartFile file,
+                                                    @RequestParam("pageNumber") int pageNumber,
                                                     @RequestParam("position") JSONObject position)
     {
         try (PDDocument document = PDDocument.load(file.getBytes()))
         {
-            // extract text from whole document
-//            PDFTextStripper stripper = new PDFTextStripper();
-//            String extractedText = stripper.getText(document);
-
-            // extract text from specific area
             Rectangle rectangle = toRectangle(position);
-            String extractedText = extractTextByArea(document, 4, rectangle);
+            String extractedText = extractTextByArea(document, pageNumber, rectangle);
 
             if (extractedText.trim().isEmpty())
             {
-//                extractedText = extractTextFromScannedImage(document);
-                extractedText = extractTextFromScannedImageByArea(document, 4, rectangle);
+                extractedText = extractTextFromScannedImageByArea(document, pageNumber, rectangle);
             }
 
             JSONObject jsonObject = new JSONObject();
-            jsonObject.put("filename", file.getOriginalFilename());
             jsonObject.put("extractedText", extractedText);
 
             return new ResponseEntity<>(jsonObject.toString(), HttpStatus.OK);
@@ -61,35 +53,6 @@ public class PDFTextExtractionController
         }
     }
 
-
-    private String extractTextFromScannedImage(PDDocument document) throws IOException, TesseractException
-    {
-        StringBuilder textToReturn = new StringBuilder();
-
-        // set up tesseract for OCR
-        ITesseract tesseract = new Tesseract();
-        tesseract.setDatapath("tessdata");
-        tesseract.setLanguage("eng");
-        tesseract.setTessVariable("user_defined_dpi", "300");
-
-        // Render each page as an Image with 300 dpi and run OCR
-        PDFRenderer pdfRenderer = new PDFRenderer(document);
-
-        for (int page = 0; page < document.getNumberOfPages(); page++)
-        {
-            BufferedImage bufferedImage = pdfRenderer.renderImageWithDPI(page, 300, ImageType.RGB);
-
-            File tempImageFile = File.createTempFile("tempFile_" + page, ".png");
-            ImageIO.write(bufferedImage,"png", tempImageFile);
-
-            String textResultFromOCR = tesseract.doOCR(tempImageFile);
-
-            textToReturn.append(textResultFromOCR);
-
-            tempImageFile.delete();
-        }
-        return textToReturn.toString();
-    }
 
     private String extractTextByArea(PDDocument document, int pageNumber, Rectangle rectangle) throws IOException
     {
@@ -113,10 +76,9 @@ public class PDFTextExtractionController
 
         // Render page as an Image with 300 dpi and run OCR
         PDFRenderer pdfRenderer = new PDFRenderer(document);
+        BufferedImage bufferedImage = pdfRenderer.renderImageWithDPI(pageNumber, 300, ImageType.RGB);
 
-        BufferedImage bufferedImage = pdfRenderer.renderImageWithDPI(pageNumber - 1, 300, ImageType.RGB);
-
-        File tempImageFile = File.createTempFile("tempFile_" + (pageNumber - 1), ".png");
+        File tempImageFile = File.createTempFile("tempFile_" + pageNumber, ".png");
         ImageIO.write(bufferedImage,"png", tempImageFile);
 
         String textResultFromOCR = tesseract.doOCR(tempImageFile, rectangle);
