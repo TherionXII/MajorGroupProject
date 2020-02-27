d035a5479a81f2041a3748333c04cb50a4feae4f
Nick <n.savkovs@gmail.com>
2020-02-27 10:20:42 +0000

    Minor updated to AppComponent

--- Web Collaboration Tool/Front-end/src/app/app.component.ts
+++ Web Collaboration Tool/Front-end/src/app/app.component.ts
@@ -1,4 +1,4 @@
-import {Component, OnInit} from '@angular/core';
+import {Component} from '@angular/core';
 import { Router } from '@angular/router';
 
 @Component({
@@ -10,10 +10,6 @@ export class AppComponent {
   public title = 'Front-end';
 
   constructor(private router: Router) {}
-
-  // public ngOnInit(): void {
-  //   localStorage.clear();
-  // }
 
   public getUsername(): string {
     return localStorage.getItem('username');
