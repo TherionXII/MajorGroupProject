efaa1943150750363b670e933001941fb934551b
Colm Doggett <colmdoggett78@gmail.com>
2020-02-28 00:54:54 +0000

    Added extraction of all images on the same page as the text to be extracted.

--- PDFProcessing/src/main/java/com/colm/pdf/PDFTextExtraction/controller/PDFTextExtractionController.java
+++ PDFProcessing/src/main/java/com/colm/pdf/PDFTextExtraction/controller/PDFTextExtractionController.java
@@ -3,8 +3,13 @@ package com.colm.pdf.PDFTextExtraction.controller;
 import net.sourceforge.tess4j.ITesseract;
 import net.sourceforge.tess4j.Tesseract;
 import net.sourceforge.tess4j.TesseractException;
+import org.apache.commons.io.FileUtils;
+import org.apache.pdfbox.cos.COSName;
 import org.apache.pdfbox.pdmodel.PDDocument;
 import org.apache.pdfbox.pdmodel.PDPage;
+import org.apache.pdfbox.pdmodel.PDResources;
+import org.apache.pdfbox.pdmodel.graphics.PDXObject;
+import org.apache.pdfbox.pdmodel.graphics.image.PDImageXObject;
 import org.apache.pdfbox.rendering.ImageType;
 import org.apache.pdfbox.rendering.PDFRenderer;
 import org.apache.pdfbox.text.PDFTextStripperByArea;
@@ -20,6 +25,9 @@ import java.awt.*;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Base64;
+import java.util.List;
 
 @RestController
 @Component
@@ -42,8 +50,11 @@ public class PDFTextExtractionController
                 extractedText = extractTextFromScannedImageByArea(document, pageNumber, rectangle);
             }
 
+            List<String> imagesOnPageBase64 = extractAllImagesFromPDFPage(document, pageNumber);
+
             JSONObject jsonObject = new JSONObject();
             jsonObject.put("extractedText", extractedText);
+            jsonObject.put("imagesOnPage", imagesOnPageBase64);
 
             return new ResponseEntity<>(jsonObject.toString(), HttpStatus.OK);
         }
@@ -96,6 +107,31 @@ public class PDFTextExtractionController
         int y2 = position.getInt("y2");
 
         return new Rectangle(x1, y1, x2-x1, y2-y1);
+    }
+
+    private List<String> extractAllImagesFromPDFPage(PDDocument document, int pageNumber) throws IOException
+    {
+        List<String> imagesOnPage = new ArrayList<>();
+
+        PDPage page = document.getPage(pageNumber);
+        PDResources pdResources = page.getResources();
+        File tempImageFile = File.createTempFile("tempFile", ".png");
+
+        for (COSName cosName : pdResources.getXObjectNames())
+        {
+            PDXObject pdxObject = pdResources.getXObject(cosName);
+            if (pdxObject instanceof PDImageXObject)
+            {
+                ImageIO.write(((PDImageXObject) pdxObject).getImage(),"PNG", tempImageFile);
+
+                byte[] fileContent = FileUtils.readFileToByteArray(tempImageFile);
+                String encodedString = Base64.getEncoder().encodeToString(fileContent);
+
+                imagesOnPage.add("data:image/png;base64," + encodedString);
+            }
+        }
+        tempImageFile.delete();
+        return imagesOnPage;
     }
 
 }
