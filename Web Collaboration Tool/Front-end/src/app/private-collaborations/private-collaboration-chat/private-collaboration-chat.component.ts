bbd16a0139e42d941c01197b25f065e409bbac27
Nick <n.savkovs@gmail.com>
2020-03-07 17:25:38 +0000

    Added ChatComponent on the front-end and implemented chat functionality

--- Web Collaboration Tool/Front-end/src/app/private-collaborations/private-collaboration-chat/private-collaboration-chat.component.ts
+++ Web Collaboration Tool/Front-end/src/app/private-collaborations/private-collaboration-chat/private-collaboration-chat.component.ts
@@ -1,15 +1,50 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnDestroy, OnInit} from '@angular/core';
+import {IMessage} from '../../Utility/Interfaces/IMessage';
+import {ActivatedRoute} from '@angular/router';
+import {FormControl} from '@angular/forms';
+import {RxStompService} from '@stomp/ng2-stompjs';
+import {Subscription} from 'rxjs';
 
 @Component({
   selector: 'app-private-collaboration-chat',
   templateUrl: './private-collaboration-chat.component.html',
   styleUrls: ['./private-collaboration-chat.component.css']
 })
-export class PrivateCollaborationChatComponent implements OnInit {
+export class PrivateCollaborationChatComponent implements OnInit, OnDestroy {
+  public messages: Array<IMessage>;
+  public username: string;
 
-  constructor() { }
+  public messageFormControl: FormControl;
+  private chatSubscription: Subscription;
 
-  ngOnInit(): void {
+  constructor(private activatedRoute: ActivatedRoute,
+              private rxStompService: RxStompService) { }
+
+  public ngOnInit(): void {
+    this.username = localStorage.getItem('username');
+
+    this.activatedRoute.data.subscribe((data: { messages: Array<IMessage> }) => this.messages = data.messages);
+
+    this.messageFormControl = new FormControl('');
+
+    this.chatSubscription = this.rxStompService.watch(`/topic/user/collaboration/chat/${this.activatedRoute.snapshot.paramMap.get('id')}`)
+                                               .subscribe(request => this.messages.push(JSON.parse(request.body) as IMessage));
   }
 
+  public ngOnDestroy(): void {
+    this.chatSubscription.unsubscribe();
+  }
+
+  public onMessageSend(): void {
+    const body = this.prepareMessageBody();
+
+    this.messageFormControl.reset('');
+
+    this.rxStompService.publish({ destination: `/app/user/collaboration/chat/${this.activatedRoute.snapshot.paramMap.get('id')}`,
+                                             body: JSON.stringify(body) })
+  }
+
+  private prepareMessageBody(): IMessage {
+    return { message: this.messageFormControl.value, sender: localStorage.getItem('username') };
+  }
 }
