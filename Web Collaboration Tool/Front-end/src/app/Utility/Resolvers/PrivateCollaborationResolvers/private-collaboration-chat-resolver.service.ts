cc74649697417d607ebdef096d05348ee5c052e3
Nick <n.savkovs@gmail.com>
2020-03-23 17:07:33 +0000

    Updated PrivateCollaborationChat to send the channel information to the component

--- Web Collaboration Tool/Front-end/src/app/Utility/Resolvers/PrivateCollaborationResolvers/private-collaboration-chat-resolver.service.ts
+++ Web Collaboration Tool/Front-end/src/app/Utility/Resolvers/PrivateCollaborationResolvers/private-collaboration-chat-resolver.service.ts
@@ -1,24 +1,26 @@
 import { Injectable } from '@angular/core';
 import {ActivatedRouteSnapshot, Resolve, RouterStateSnapshot} from '@angular/router';
 import {IMessage} from '../../Interfaces/IMessage';
-import {EMPTY, Observable} from 'rxjs';
+import {EMPTY, forkJoin, Observable} from 'rxjs';
 import {ThreadService} from '../../../PrivateCollaboration/Services/thread.service';
 import {catchError, first} from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
 })
-export class PrivateCollaborationChatResolverService implements Resolve<Array<IMessage>> {
+export class PrivateCollaborationChatResolverService implements Resolve<[Array<IMessage>, string, string]> {
   constructor(private threadService: ThreadService) {}
 
-  resolve(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): Observable<Array<IMessage>> | Observable<never> {
-    return this.threadService.getMessagesForThread(route.paramMap.get('id'))
-                             .pipe(first(), catchError(() => this.onFail()))
+  public resolve(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): Observable<[Array<IMessage>, string, string]> | Observable<never> {
+    return forkJoin([
+      this.threadService.getMessagesForThread(route.paramMap.get('id'))
+                             .pipe(first(), catchError(() => this.onFail())),
+      `/topic/user/collaboration/chat/${route.paramMap.get('id')}`,
+      `/app/user/collaboration/chat/${route.paramMap.get('id')}`
+    ]);
   }
 
   private onFail(): Observable<never> {
-    console.log('Failed to retrieve messages');
-
     return EMPTY;
   }
 }
