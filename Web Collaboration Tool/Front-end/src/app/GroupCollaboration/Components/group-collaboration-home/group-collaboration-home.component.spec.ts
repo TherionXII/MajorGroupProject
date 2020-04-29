2b8f9782265c88ca4bdb704e44e848bb4af39ffd
Nick <n.savkovs@gmail.com>
2020-04-29 21:07:23 +0100

    Implemented tests for GroupCollaborationHome; refactor

--- Web Collaboration Tool/Front-end/src/app/GroupCollaboration/Components/group-collaboration-home/group-collaboration-home.component.spec.ts
+++ Web Collaboration Tool/Front-end/src/app/GroupCollaboration/Components/group-collaboration-home/group-collaboration-home.component.spec.ts
@@ -1,14 +1,16 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import {CUSTOM_ELEMENTS_SCHEMA} from '@angular/core';
 
 import { GroupCollaborationHomeComponent } from './group-collaboration-home.component';
 
-describe('GroupCollaborationPageComponent', () => {
+fdescribe('GroupCollaborationPageComponent', () => {
   let component: GroupCollaborationHomeComponent;
   let fixture: ComponentFixture<GroupCollaborationHomeComponent>;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ GroupCollaborationHomeComponent ]
+      declarations: [ GroupCollaborationHomeComponent ],
+      schemas: [ CUSTOM_ELEMENTS_SCHEMA ]
     })
     .compileComponents();
   }));
