fa33b8fb675b7d32fbdfa4d39b1daf82ccc89b14
Nick <n.savkovs@gmail.com>
2020-04-29 21:07:02 +0100

    Implemented tests for group component; refactor

--- Web Collaboration Tool/Front-end/src/app/GroupCollaboration/Components/group/group.component.ts
+++ Web Collaboration Tool/Front-end/src/app/GroupCollaboration/Components/group/group.component.ts
@@ -1,8 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-import {IGroup} from '../../Interfaces/IGroup';
-import {ActivatedRoute} from '@angular/router';
-import {IPrivateCollaboration} from '../../../PrivateCollaboration/Interfaces/IPrivateCollaboration';
-import {IGroupCollaborationRequest} from '../../../Utility/Interfaces/IGroupCollaborationRequest';
 
 @Component({
   selector: 'app-group',
@@ -10,23 +6,7 @@ import {IGroupCollaborationRequest} from '../../../Utility/Interfaces/IGroupColl
   styleUrls: ['./group.component.css']
 })
 export class GroupComponent implements OnInit {
-  public group: IGroup;
-  public groupInvitations: Array<IGroupCollaborationRequest>;
-  public privateCollaborations: Array<IPrivateCollaboration>;
+  constructor() {}
 
-  public username: string;
-
-  constructor(private activatedRoute: ActivatedRoute) {}
-
-  public ngOnInit(): void {
-    this.activatedRoute.data.subscribe((data: { groupData: IGroup, privateCollaborations: Array<IPrivateCollaboration> }) => {
-      this.group = data.groupData[0];
-      this.groupInvitations = data.groupData[1];
-      this.privateCollaborations = data.privateCollaborations;
-    });
-
-    this.username = localStorage.getItem('username');
-  }
-
-
+  public ngOnInit(): void {}
 }
