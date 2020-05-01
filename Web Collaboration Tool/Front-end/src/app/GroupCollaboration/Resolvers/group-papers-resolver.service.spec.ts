871f7c2e6df734f1b137273b5641812402d8957d
Nick <n.savkovs@gmail.com>
2020-05-01 14:11:06 +0100

    Implememnted tests for GroupPapers resolver

--- Web Collaboration Tool/Front-end/src/app/GroupCollaboration/Resolvers/group-papers-resolver.service.spec.ts
+++ Web Collaboration Tool/Front-end/src/app/GroupCollaboration/Resolvers/group-papers-resolver.service.spec.ts
@@ -1,16 +1,51 @@
 import { TestBed } from '@angular/core/testing';
 
 import { GroupPapersResolverService } from './group-papers-resolver.service';
+import {RouterTestingModule} from '@angular/router/testing';
+import {IPaper} from '../../Paper/Interfaces/IPaper';
+import {PaperService} from '../../Paper/Services/paper.service';
+import {ActivatedRoute, Router} from '@angular/router';
+import {Type} from '@angular/core';
+import {of, throwError} from 'rxjs';
 
-describe('GroupPapersResolverService', () => {
+fdescribe('GroupPapersResolverService', () => {
   let service: GroupPapersResolverService;
 
+  const mockPapers = [ {} as IPaper, {} as IPaper ];
+  const paperServiceStub = jasmine.createSpyObj('PaperService', [ 'getPapersForGroup' ]);
+
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      imports: [ RouterTestingModule ],
+      providers: [ { provide: PaperService, useValue: paperServiceStub } ]
+    });
     service = TestBed.inject(GroupPapersResolverService);
+  });
+
+  beforeEach(() => {
+    spyOn(TestBed.inject(ActivatedRoute as Type<ActivatedRoute>).snapshot.paramMap, 'get').and.returnValue('0');
   });
 
   it('should be created', () => {
     expect(service).toBeTruthy();
+  });
+
+  it('should return valid data when successful', () => {
+    paperServiceStub.getPapersForGroup.and.returnValue(of(mockPapers));
+
+    service.resolve(TestBed.inject(ActivatedRoute).snapshot, TestBed.inject(Router).routerState.snapshot)
+      .subscribe(data => {
+        expect(paperServiceStub.getPapersForGroup).toHaveBeenCalledWith('0');
+
+        expect(data).toEqual(mockPapers);
+      }, () => fail('Should have succeeded!'));
+  });
+
+  it('should return an error when request failed', () => {
+    paperServiceStub.getPapersForGroup.and.returnValue(throwError('Error'));
+
+    service.resolve(TestBed.inject(ActivatedRoute).snapshot, TestBed.inject(Router).routerState.snapshot)
+      .subscribe(() => fail('Should have failed!'),
+                 error => expect(error).toEqual('Failed to retrieve exam papers; please try again later'));
   });
 });
--- Web Collaboration Tool/Front-end/src/app/GroupCollaboration/Resolvers/group-papers-resolver.service.ts
+++ Web Collaboration Tool/Front-end/src/app/GroupCollaboration/Resolvers/group-papers-resolver.service.ts
@@ -1,24 +1,18 @@
 import { Injectable } from '@angular/core';
-import {ActivatedRouteSnapshot, Resolve, Router, RouterStateSnapshot} from '@angular/router';
+import {ActivatedRouteSnapshot, Resolve, RouterStateSnapshot} from '@angular/router';
 import {IPaper} from '../../Paper/Interfaces/IPaper';
-import {NEVER, Observable} from 'rxjs';
+import {Observable, throwError} from 'rxjs';
 import {PaperService} from '../../Paper/Services/paper.service';
 import {catchError, first} from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
 })
-export class GroupPapersResolverService implements Resolve<Array<IPaper>>{
-  constructor(private paperService: PaperService, private router: Router) { }
+export class GroupPapersResolverService implements Resolve<Array<IPaper>> {
+  constructor(private paperService: PaperService) {}
 
   public resolve(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): Observable<Array<IPaper>>  {
     return this.paperService.getPapersForGroup(route.paramMap.get('groupId'))
-      .pipe(first(), catchError(error => this.onFail(error)));
-  }
-
-  private onFail(error: any): Observable<never> {
-    this.router.navigateByUrl('/');
-
-    return NEVER;
+      .pipe(first(), catchError(() => throwError('Failed to retrieve exam papers; please try again later')));
   }
 }
