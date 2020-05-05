484d83c891d4572981355d17cde7315758d9fb7b
Nick <n.savkovs@gmail.com>
2020-05-05 14:32:51 +0100

    Tested PrivateCollaboration service; removed redundant Request service

--- Web Collaboration Tool/Front-end/src/app/PrivateCollaboration/Services/private-collaboration.service.ts
+++ Web Collaboration Tool/Front-end/src/app/PrivateCollaboration/Services/private-collaboration.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import {HttpClient} from '@angular/common/http';
 import {Observable} from 'rxjs';
 import {IPrivateCollaboration} from '../Interfaces/IPrivateCollaboration';
+import {IPrivateCollaborationRequest} from '../../Utility/Interfaces/IPrivateCollaborationRequest';
 
 @Injectable({
   providedIn: 'root'
@@ -13,7 +14,11 @@ export class PrivateCollaborationService {
     return this.httpClient.get<boolean>(`http://localhost:8080/isCollaborating/${firstUsername}/${secondUsername}`);
   }
 
-  public getAllPrivateCollaborationsForUser(username: string): Observable<Array<IPrivateCollaboration>> {
+  public getPrivateCollaborationsForUser(username: string): Observable<Array<IPrivateCollaboration>> {
     return this.httpClient.get<Array<IPrivateCollaboration>>(`http://localhost:8080/privateCollaborations/${username}`);
+  }
+
+  public getPrivateCollaborationRequestsForUser(username: string): Observable<Array<IPrivateCollaborationRequest>> {
+    return this.httpClient.get<Array<IPrivateCollaborationRequest>>(`http://localhost:8080/getPrivateCollaborationRequests/${username}`);
   }
 }
