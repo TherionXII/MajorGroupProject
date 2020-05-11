58de5d7816ef1e987820b5aaecd39c5192baa4db
Nick <n.savkovs@gmail.com>
2020-05-11 18:24:49 +0100

    Tested GroupMember entity

--- Web Collaboration Tool/Back-end/src/main/java/project/webcollaborationtool/Collaboration/GroupCollaboration/Entities/GroupMember.java
+++ Web Collaboration Tool/Back-end/src/main/java/project/webcollaborationtool/Collaboration/GroupCollaboration/Entities/GroupMember.java
@@ -1,15 +1,17 @@
 package project.webcollaborationtool.Collaboration.GroupCollaboration.Entities;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
-import lombok.Data;
+import lombok.*;
 import project.webcollaborationtool.User.Entities.User;
 import project.webcollaborationtool.Utility.CompositeKeys.GroupMemberId;
 
 import javax.persistence.*;
 import javax.validation.constraints.NotNull;
 
-@Data
 @Entity
+@ToString
+@Getter @Setter
+@NoArgsConstructor
 @IdClass(GroupMemberId.class)
 public class GroupMember
 {
@@ -19,12 +21,14 @@ public class GroupMember
     @Id
     private String memberUsername;
 
+    @NotNull
     @ManyToOne
     @JsonIgnore
     @MapsId("groupId")
     @JoinColumn(name = "groupId", referencedColumnName = "id")
     private GroupCollaboration groupCollaboration;
 
+    @NotNull
     @ManyToOne
     @JsonIgnore
     @MapsId("memberUsername")
---
+++ Web Collaboration Tool/Back-end/src/test/java/project/webcollaborationtool/Collaboration/GroupCollaboration/Entities/GroupMemberEntityTests.java
@@ -0,0 +1,78 @@
+package project.webcollaborationtool.Collaboration.GroupCollaboration.Entities;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import project.webcollaborationtool.User.Entities.Profile;
+import project.webcollaborationtool.User.Entities.User;
+
+import java.util.ArrayList;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@DataJpaTest
+@ExtendWith(SpringExtension.class)
+public class GroupMemberEntityTests
+{
+    @Test
+    public void testGroupMemberEntityConstructor()
+    {
+        var groupMember = new GroupMember();
+        assertThat(groupMember).isNotNull();
+    }
+
+    @Test
+    public void testGettersAndSetters()
+    {
+        var groupMember = new GroupMember();
+
+        groupMember.setGroupId(0);
+        assertThat(groupMember.getGroupId()).isEqualTo(0);
+
+        groupMember.setMemberUsername("username");
+        assertThat(groupMember.getMemberUsername()).isEqualTo("username");
+
+        groupMember.setGroupCollaboration(this.getMockGroup());
+        assertThat(groupMember.getGroupCollaboration()).isNotNull();
+        assertThat(groupMember.getGroupCollaboration().getId()).isEqualTo(0);
+
+        groupMember.setMember(this.getMockUser());
+        assertThat(groupMember.getMember()).isNotNull();
+        assertThat(groupMember.getMember().getUsername()).isEqualTo("username");
+
+        groupMember.setIsAdmin(true);
+        assertThat(groupMember.getIsAdmin()).isTrue();
+    }
+
+    @Test
+    public void testToString()
+    {
+        var groupMember = new GroupMember();
+        assertThat(groupMember.toString().contains("GroupMember")).isTrue();
+    }
+
+    private GroupCollaboration getMockGroup()
+    {
+        var groupCollaboration = new GroupCollaboration();
+        groupCollaboration.setId(0);
+        groupCollaboration.setGroupMembers(new ArrayList<>());
+        groupCollaboration.setExamPapers(new ArrayList<>());
+
+        return groupCollaboration;
+    }
+
+    private User getMockUser()
+    {
+        var user = new User();
+        user.setUsername("username");
+        user.setPassword("password");
+
+        var profile = new Profile();
+        profile.setUsername(user.getUsername());
+
+        user.setProfile(profile);
+
+        return user;
+    }
+}
